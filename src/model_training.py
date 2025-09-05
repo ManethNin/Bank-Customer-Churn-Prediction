82ef15e914b282202783bef382d9b418460b28e4
ManethNin <kgmninduwara@gmail.com>
2025-09-05 13:19:40 +0530

    model training class

---
+++ src/model_training.py
@@ -0,0 +1,16 @@
+import os
+import pickle
+import joblib
+
+
+class ModelTrainer:
+    def train_simple(self, model, X_train, y_train):
+        model.fit(X_train, y_train)
+        train_score = model.score(X_train, y_train)
+        return model, train_score
+
+    def save_model(self, model, filepath):
+        joblib.dump(model, filepath)
+
+    def load_model(self, filepath):
+        return joblib.load(filepath)
