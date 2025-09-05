c12909ae7d0a843fd72aedf66c177b3cb2bd70ce
ManethNin <kgmninduwara@gmail.com>
2025-09-05 23:56:25 +0530

    model training class

--- src/model_training.py
+++ src/model_training.py
@@ -4,7 +4,7 @@ import joblib
 
 
 class ModelTrainer:
-    def train_simple(self, model, X_train, y_train):
+    def train(self, model, X_train, y_train):
         model.fit(X_train, y_train)
         train_score = model.score(X_train, y_train)
         return model, train_score
