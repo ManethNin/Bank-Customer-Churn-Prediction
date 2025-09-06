6dfc0dcc2b0f4d018f53b202f6380fcec918001a
ManethNin <kgmninduwara@gmail.com>
2025-09-06 10:24:57 +0530

    add evaluator to the pipeline and test

---
+++ pipelines/training_pipeline.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import logging
+import pandas as pd
+import joblib
+from typing import Dict, Any, Tuple, Optional
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+
+from data_pipeline import data_pipeline
+from model_building import XGboostModelBuilder, RandomForestModelBuilder
+from model_training import ModelTrainer
+from model_evaluation import ModelEvaluator
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'utils'))
+
+from config import get_model_config, get_data_paths
+logging.basicConfig(level=logging.INFO, format=
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+def training_pipeline(  data_path: str='../data/raw/ChurnModelling.csv', model_params: Optional[Dict[str, Any]] = None,test_size: float=0.2, random_state : int=42,model_path: str = '../artifacts/models/random_forest_cv_model.joblib'):
+
+    if (not os.path.exists('../'+get_data_paths()['data_artifacts_dir'] +'/X_train.csv')) or \
+        (not os.path.exists('../'+get_data_paths()['data_artifacts_dir']+ '/Y_train.csv')) or \
+        (not os.path.exists('../'+get_data_paths()['data_artifacts_dir'] + '/X_test.csv')) or \
+        (not os.path.exists('../'+get_data_paths()['data_artifacts_dir'] + '/Y_test.csv')):
+
+        data_pipeline()
+
+    else:
+        print("Loading..")
+
+
+    X_train = pd.read_csv('../'+get_data_paths()['data_artifacts_dir'] +'/X_train.csv')
+    Y_train = pd.read_csv('../'+get_data_paths()['data_artifacts_dir'] +'/Y_train.csv')
+    X_test = pd.read_csv('../'+get_data_paths()['data_artifacts_dir'] +'/X_test.csv')
+    Y_test = pd.read_csv('../'+get_data_paths()['data_artifacts_dir'] +'/Y_test.csv')
+
+
+    model_builder = XGboostModelBuilder()
+    model = model_builder.build_model()
+
+    trainer = ModelTrainer()
+    model, score = trainer.train(model=model, X_train= X_train,y_train= Y_train.squeeze())
+    # print(score)
+
+    evaluator = ModelEvaluator(model, 'XGboost')
+
+    evaluator.evaluate(X_test=X_test, Y_test= Y_test)
+
+
+training_pipeline()
